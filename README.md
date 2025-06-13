ee9d6e8528f1af5206a8ccba06ca98c12c3c1dfe
Roman Kharitonov <rkharito@yandex.ru>
2025-06-13 22:40:53 +1000

    Support item sorting with SortBags addon

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-## Fork details ##
+## Fork details
 
-A search bar has been added that allows you to filter items in your inventory, bank and keyring.    
-The inventory and bank will also remember their positions when moved.
+- A search bar has been added that allows you to filter items in your inventory, bank and keyring.
+- The inventory and bank will also remember their positions when moved.
+- Added support for item sorting with [SortBags](https://github.com/shirsig/SortBags-vanilla) addon.
 
 ![SUCCbag](https://user-images.githubusercontent.com/107083057/234946553-9c0dfd94-ef42-4c34-8b6d-e9b99732fa20.png)
 
@@ -10,7 +11,6 @@ Sublime User Convenience Collection - player inventory and bank
 
 ![succ-bag](https://cloud.githubusercontent.com/assets/17740865/23642754/f5d4baa6-02b1-11e7-89ad-db77a9b3a6c3.jpg)
 
-Sorting feature with [Clean_Up](https://github.com/GryllsAddons/Clean_Up) addon
+Sorting feature with [SortBags](https://github.com/shirsig/SortBags-vanilla) or [Clean_Up](https://github.com/GryllsAddons/Clean_Up) addon
 
 Configurations available at /succbag
-
--- SUCC-bag.lua
+++ SUCC-bag.lua
@@ -1,3 +1,21 @@
+local function isSortingAddonLoaded()
+	return SortBags ~= nil and SortBankBags ~= nil or Clean_Up ~= nil
+end
+
+local function sortItems(where, reversed)
+	if SortBags ~= nil and SortBankBags ~= nil then
+		local oldReversed = GetSortBagsRightToLeft()
+		SetSortBagsRightToLeft(reversed)
+		if where == "bags" then
+			SortBags()
+		elseif where == "bank" then
+			SortBankBags()
+		end
+		SetSortBagsRightToLeft(oldReversed)
+	elseif Clean_Up ~= nil then
+		Clean_Up(where, reversed)
+	end
+end
 
 function SUCC_bagDefaults()
 	SUCC_bagOptions = {}
@@ -550,7 +568,7 @@ local function Essentials(frame)
 			GameTooltip:Show()
 		end)
 		frame.toggleButton:SetScript('OnLeave', function() GameTooltip:Hide() end)
-		if Clean_Up then
+		if isSortingAddonLoaded() then
 			frame.cuBag = CreateFrame('Button', t .. 'CU_button', frame)
 			frame.cuBag:SetHeight(12)
 			frame.cuBag:SetWidth(12)
@@ -562,11 +580,7 @@ local function Essentials(frame)
 			frame.cuBag:RegisterForClicks('LeftButtonUp', 'RightButtonUp')
 			frame.cuBag:SetScript('OnClick', function()
 				local c = frame.title.t ~= '' and frame.title.t ~= nil and string.lower(frame.title.t) or 'bags'
-				if arg1 == 'RightButton' then
-					Clean_Up(c, 1)
-				else
-					Clean_Up(c)
-				end
+				sortItems(c, arg1 == 'RightButton')
 			end)
 			frame.cuBag:SetScript('OnEnter', function()
 				GameTooltip:SetOwner(this, 'ANCHOR_LEFT')
@@ -1068,7 +1082,7 @@ local function CreateMenuFrame()
 	menu.override.t:SetWidth(200)
 	menu.override.t:SetText('Quality color above bag color:')
 
-	if Clean_Up then
+	if isSortingAddonLoaded() then
 		menu.cleanup = CreateFrame('CheckButton', 'SBC_cleanUp', menu, 'UICheckButtonTemplate')
 		menu.cleanup:SetHeight(25)
 		menu.cleanup:SetWidth(25)
